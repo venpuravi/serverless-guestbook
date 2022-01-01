f0c6185ff6c43348e1e3302ffc6a404432e2a5c8
venpuravi <venpuravi@gmail.com>
2022-01-01 06:31:47 -0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://01f83025.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
